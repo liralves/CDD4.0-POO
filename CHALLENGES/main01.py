8ccb9050311bac80158a5fd876e4ae7d8bcd0d40
Luana Lira <154566662+liralves@users.noreply.github.com>
2024-05-19 12:43:21 -0300

    main01.py

--- CHALLENGES/main01.py
+++ CHALLENGES/main01.py
@@ -1,7 +1,7 @@
 from classes import *
 P1 = Person("", "", "")
 OP = 0
-P1.name = input("Digite o nome da pessoa: ")
+P1.name = input("Digite o nome do personagem: ")
 while OP != 7:
     OP = int(input(f"O que {P1.name} irá fazer? \n1.COMER | 2.FALAR | 3.DORMIR | 4.PARAR DE COMER | 5.PARAR DE FALAR | 6.PARAR DE DORMIR | 7.SAIR\n"))
     while OP < 1 or OP > 7:
