6a93bd4e75606623db76ba3aa348e5cb616f0ae8
Luana Lira <154566662+liralves@users.noreply.github.com>
2024-05-27 09:52:07 -0300

    CHALLENGE02.py

--- CHALLENGES/CHALLENGE02.py
+++ CHALLENGES/CHALLENGE02.py
@@ -1,28 +1,28 @@
 class Jokenpo:
-  def __init__(self, win):
-    self.win = win
+    def __init__(self, win):
+        self.win = win
 
-def move_result(self, name, player, computer):
-  if computer == player:
-    print("Empate!")
-    self.win = False
-  elif computer == 'Pedra':
-    if player == 'Papel':
-      self.win = True
-    elif player == 'Tesoura':
-      self.win = False
-  elif computer == 'Papel':
-      if player == 'Tesoura':
-        self.win = True
-      elif player == 'Pedra':
-        self.win = False
-  elif computer == 'Tesoura':
-      if player == 'Pedra':
-        self.win = True
-      elif player == 'Papel':
-        self.win = False
+    def move_result(self, computer, player, name):
+        if computer == player:
+            print("Empate!")
+        else:
+            if computer == 'Pedra':
+                if player == 'Papel':
+                    self.win = True
+                elif player == 'Tesoura':
+                    self.win = False
+            elif computer == 'Papel':
+                if player == 'Tesoura':
+                    self.win = True
+                elif player == 'Pedra':
+                    self.win = False
+            elif computer == 'Tesoura':
+                if player == 'Pedra':
+                    self.win = True
+                elif player == 'Papel':
+                    self.win = False
 
-  if self.win == False:
-    print("COMPUTER WINS!")
-  else:
-    print(f"{name} WINS!")
+            if self.win:
+                print("COMPUTER WINS!")
+            else:
+                print(f"{name} WINS!")
